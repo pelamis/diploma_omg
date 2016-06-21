98ced64722969e9450def1a62f2a63dce973385f
Pelamis <poisondeluxe@gmail.com>
2016-06-21 08:09:45 +0300

    Callback.h added

---
+++ callbacks.h
@@ -0,0 +1,18 @@
+//#ifndef CALLBACKS_H_
+//#define CALLBACKS_H_
+//
+//#include "commons.h"
+//#include "cudakernels.h"
+//
+//#define REFRESH 10
+//
+//bool animate = false;
+//
+//int A = 0;
+//int stepA = 10;
+//
+//void display();
+//void timerEvent(int value);
+//void keyboard(unsigned char key, int /*x*/, int /*y*/);
+//void reshape(int x, int y);
+//#endif
